359197a0f9a8721f3bdde8bd0446805fdd0658c3
nishant16101 <nishantkumbhar812@gmail.com>
2025-01-05 13:50:34 +0530

    Data Transformation

--- src/datasciencproject/config/configuration.py
+++ src/datasciencproject/config/configuration.py
@@ -1,4 +1,4 @@
-from src.datasciencproject.entity.config_entity import (DataIngestionConfig,DataValidationConfig)
+from src.datasciencproject.entity.config_entity import (DataIngestionConfig,DataValidationConfig,DataTransformationConfig)
 from src.datasciencproject.constants import *
 from src.datasciencproject.utils.common import read_yaml,create_directories
 
@@ -33,3 +33,11 @@ class ConfigurationManager:
             all_schema=schema
         )
         return data_validation_config
+    def get_data_transformation_config(self)->DataTransformationConfig:
+        config = self.config.data_transformation
+        create_directories([config.root_dir])
+        data_transformation_config =DataTransformationConfig(
+            root_dir=config.root_dir,
+            data_path=config.data_path
+        )
+        return data_transformation_config
